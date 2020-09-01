f62230685b4aec5ec4595ff28b8c034c75044eb0
Jake Watson <44416456+Jake1402@users.noreply.github.com>
2020-09-01 22:06:42 +0100

    Add files via upload

---
+++ HuishFormProject/HuishFormProject/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmDiceCricket
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDiceCricket))
+        Me.lblPlayerName = New System.Windows.Forms.Label()
+        Me.InputName = New System.Windows.Forms.TextBox()
+        Me.btnPlayerName = New System.Windows.Forms.Button()
+        Me.btnNextShot = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtScores = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.totalBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblPlayerName
+        '
+        resources.ApplyResources(Me.lblPlayerName, "lblPlayerName")
+        Me.lblPlayerName.Name = "lblPlayerName"
+        '
+        'InputName
+        '
+        Me.InputName.BackColor = System.Drawing.SystemColors.InactiveBorder
+        resources.ApplyResources(Me.InputName, "InputName")
+        Me.InputName.Name = "InputName"
+        '
+        'btnPlayerName
+        '
+        resources.ApplyResources(Me.btnPlayerName, "btnPlayerName")
+        Me.btnPlayerName.Name = "btnPlayerName"
+        Me.btnPlayerName.UseVisualStyleBackColor = True
+        '
+        'btnNextShot
+        '
+        resources.ApplyResources(Me.btnNextShot, "btnNextShot")
+        Me.btnNextShot.Name = "btnNextShot"
+        Me.btnNextShot.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'txtScores
+        '
+        resources.ApplyResources(Me.txtScores, "txtScores")
+        Me.txtScores.Name = "txtScores"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'totalBox
+        '
+        resources.ApplyResources(Me.totalBox, "totalBox")
+        Me.totalBox.Name = "totalBox"
+        '
+        'frmDiceCricket
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.totalBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtScores)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnNextShot)
+        Me.Controls.Add(Me.btnPlayerName)
+        Me.Controls.Add(Me.InputName)
+        Me.Controls.Add(Me.lblPlayerName)
+        Me.HelpButton = True
+        Me.MaximizeBox = False
+        Me.Name = "frmDiceCricket"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPlayerName As Label
+    Friend WithEvents InputName As TextBox
+    Friend WithEvents btnPlayerName As Button
+    Friend WithEvents btnNextShot As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtScores As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents totalBox As TextBox
+End Class
---
+++ HuishFormProject/HuishFormProject/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.ComponentModel
+
+Public Class frmDiceCricket
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub ClearArr()
+
+    End Sub
+
+    '
+    'Global variables
+    '
+
+    Dim PlaceInNames As Integer = 0
+    Dim RollPlaces As Integer = 0
+    Dim TotalDiceValueA As Integer = 0
+    Dim TotalDiceValueB As Integer = 0
+
+    Dim NameArray(1) As String
+    Dim Score(1024) As Integer
+
+    '
+    'Class to add players
+    '
+
+    Private Sub btnPlayerName_MouseClick(sender As Object, e As MouseEventArgs) Handles btnPlayerName.MouseClick
+
+        NameArray(PlaceInNames) = InputName.Text
+        txtScores.AppendText(NameArray(PlaceInNames) + " - Has entered!" & vbCrLf) '& vbCrLf &
+
+        PlaceInNames += 1
+
+    End Sub
+
+    '
+    ' Class that detects weather someone has rolled a dice
+    '
+
+    Private Sub btnNextShot_MouseClick(sender As Object, e As MouseEventArgs) Handles btnNextShot.MouseClick
+
+        Dim DiceRoll As Integer = Math.Ceiling(Rnd() * 12)
+        Score(RollPlaces) = DiceRoll
+
+        If RollPlaces Mod 2 = 0 Then
+
+            If DiceRoll = 5 Then
+                txtScores.AppendText(NameArray(0) & " is a LOSER" & vbCrLf)
+                totalBox.Text = NameArray(1) & " won with " & TotalDiceValueB.ToString() & " points"
+
+            Else
+                txtScores.AppendText(NameArray(0) & " has rolled a " & DiceRoll.ToString() & vbCrLf)
+                TotalDiceValueA += DiceRoll
+                totalBox.Text = NameArray(0) & " your score is: " & TotalDiceValueA.ToString()
+
+
+            End If
+
+        Else
+
+            If DiceRoll = 5 Then
+                txtScores.AppendText(NameArray(1) & " is a LOSER" & vbCrLf)
+                totalBox.Text = NameArray(0) & " won with " & TotalDiceValueA.ToString() & " points"
+
+
+            Else
+                txtScores.AppendText(NameArray(1) & " has rolled a " & DiceRoll.ToString() & vbCrLf)
+                TotalDiceValueB += DiceRoll
+                totalBox.Text = NameArray(1) & " your score is: " & TotalDiceValueB.ToString()
+
+            End If
+
+        End If
+
+        RollPlaces += 1
+
+    End Sub
+End Class
